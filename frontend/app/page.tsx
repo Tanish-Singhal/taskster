33e4e36b861542e1e69f09adec76177bc022fb91
Tanish Singhal <tanishsinghal510@gmail.com>
2024-10-11 21:11:06 +0530

    forgot to add

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 // import Banner from "./components/Banner";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Users from "./components/Users";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       {/* <Banner /> */}
       <Navbar />
       <Hero />
+      <Users />
     </div>
   );
 }

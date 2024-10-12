08f1fcb8af48f5bccdeadf2f48e502c3b5c9b301
Tanish Singhal <tanishsinghal510@gmail.com>
2024-10-13 00:44:17 +0530

    Pricing component added

---
+++ frontend/app/components/Pricing.tsx
@@ -0,0 +1,44 @@
+import AnimationContainer from "@/components/animation-container";
+import { Button } from "@/components/ui/button";
+import MagicBadge from "@/components/ui/magic-badge";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div>
+      <AnimationContainer delay={0.1}>
+        <div className="py-28 pt-14 bg-neutral-950 flex flex-col items-center justify-center text-center text-white">
+          <MagicBadge title="Pricing" />
+
+          <h2 className="text-center lg:text-center text-3xl md:text-5xl leading-[1.1] font-medium font-heading text-foreground mt-6 sm:mt-8 text-white">
+            Taskster is Completely Free for Everyone!
+          </h2>
+          <p className="mt-4 text-center lg:text-center text-lg text-muted-foreground max-w-screen-md">
+            Enjoy full access to all of Taskster&apos;s powerful features at no cost—no hidden fees or
+            premium plans! As an open-source, community-driven project, we encourage you to
+            contribute and customize Taskster to meet your needs.
+          </p>
+
+          <AnimationContainer delay={0.1}>
+            <div className="py-10 md:py-12">
+              <Button variant="secondary" className="group font-semibold">
+                <Link
+                  href="https://github.com/Tanish-Singhal/taskster"
+                  target="_blank"
+                  className="flex items-center"
+                >
+                  Github Repository
+                  <ArrowRight className="size-4 ml-2 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
+                </Link>
+              </Button>
+            </div>
+          </AnimationContainer>
+        </div>
+      </AnimationContainer>
+    </div>
+  );
+};
+
+export default Pricing;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,16 +2,18 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 import Users from "./components/Users";
 
 export default function Home() {
   return (
-    <div className="bg-neutral-950 overflow-x-hidden scrollbar-hide">
+    <div className="bg-neutral-950 overflow-hidden scrollbar-hide">
       {/* <Banner /> */}
       <Navbar />
       <Hero />
       <Users />
       <Features />
+      <Pricing />
     </div>
   );
 }

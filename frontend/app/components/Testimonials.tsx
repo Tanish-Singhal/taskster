6d3ba1427bbd7e0ade1194b63bb9b040c54846ed
Tanish Singhal <tanishsinghal510@gmail.com>
2024-10-14 01:32:20 +0530

    testimonial added

---
+++ frontend/app/components/Testimonials.tsx
@@ -0,0 +1,29 @@
+import AnimationContainer from "@/components/animation-container";
+import MagicBadge from "@/components/ui/magic-badge";
+import React from "react";
+import { MarqueeDemo } from "./MarqueeDemo";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <AnimationContainer delay={0.1}>
+        <div className="py-28 pt-14 bg-neutral-950 flex flex-col items-center justify-center text-center text-white">
+          <MagicBadge title="Testimonials" />
+
+          <h2 className="text-center lg:text-center text-3xl md:text-5xl leading-[1.1] font-medium font-heading text-foreground mt-6 sm:mt-8 text-white">
+            What our users are saying
+          </h2>
+          <p className="mt-4 text-center lg:text-center text-lg text-muted-foreground max-w-screen-md">
+            Here's what some of our users have to say about Taskster.
+          </p>
+
+          <AnimationContainer delay={0.2}>
+            <MarqueeDemo />
+          </AnimationContainer>
+        </div>
+      </AnimationContainer>
+    </div>
+  );
+};
+
+export default Testimonials;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -65,7 +65,9 @@ const config: Config = {
   		},
   		animation: {
   			shine: 'shine var(--duration) infinite linear',
-  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear'
+  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+  			marquee: 'marquee var(--duration) infinite linear',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
   		},
   		keyframes: {
   			shine: {
@@ -82,6 +84,22 @@ const config: Config = {
   			'border-beam': {
   				'100%': {
   					'offset-distance': '100%'
+  				}
+  			},
+  			marquee: {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
   				}
   			}
   		}

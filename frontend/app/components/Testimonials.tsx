4139b2d7becf19dcd9bf632f2850758b33465bf1
Tanish Singhal <tanishsinghal510@gmail.com>
2024-10-15 16:37:54 +0530

    some changes were made

--- frontend/app/components/Testimonials.tsx
+++ frontend/app/components/Testimonials.tsx
@@ -7,14 +7,14 @@ const Testimonials = () => {
   return (
     <div>
       <AnimationContainer delay={0.1}>
-        <div className="py-32 pt-14 bg-neutral-950 flex flex-col items-center justify-center text-center text-white">
+        <div className="sm:py-28 py-12 bg-neutral-950 flex flex-col items-center justify-center text-center text-white">
           <MagicBadge title="Testimonials" />
 
           <h2 className="text-center lg:text-center text-3xl md:text-5xl leading-[1.1] font-medium font-heading text-foreground mt-6 sm:mt-8 text-white">
             What our users are saying
           </h2>
           <p className="mt-4 text-center lg:text-center text-lg text-muted-foreground max-w-screen-md">
-            Here&apos;s what some of our users have to say about Taskster. {/* Escape single quote */}
+            Here&apos;s what some of our users have to say about Taskster.
           </p>
 
           <AnimationContainer delay={0.2}>

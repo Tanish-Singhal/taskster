a00ad3a763397461fcb92745c0411f57799947ee
Tanish Singhal <tanishsinghal510@gmail.com>
2024-10-17 19:25:33 +0530

    fix error

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -7,28 +7,30 @@ export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="absolute left-1/2 top-1/2 mb-16 -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center bg-white dark:bg-neutral-950">
-      <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
-        404
-      </span>
-      <h2 className="font-heading my-2 text-2xl font-bold text-neutral-800 dark:text-neutral-200">
-        Something&apos;s missing
-      </h2>
-      <p className='text-muted-foreground dark:text-neutral-400'>
-        Sorry, the page you are looking for doesn&apos;t exist or has been
-        moved.
-      </p>
-      <div className="mt-10 flex justify-center gap-2">
-        <Button onClick={() => router.back()} variant="default" size="lg">
-          Go back
-        </Button>
-        <Button
-          onClick={() => router.push('/dashboard')}
-          variant="ghost"
-          size="lg"
-        >
-          Back to Home
-        </Button>
+    <div className="absolute inset-0 bg-neutral-950 flex items-center justify-center text-center">
+      <div className="mb-16">
+        <span className="bg-gradient-to-b from-white to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
+          404
+        </span>
+        <h2 className="font-heading my-2 text-2xl font-bold text-neutral-200">
+          Something&apos;s missing
+        </h2>
+        <p className='text-neutral-400 max-w-screen-md'>
+          Sorry, the page you are looking for doesn&apos;t exist or has been
+          moved.
+        </p>
+        <div className="mt-10 flex justify-center gap-2">
+          <Button onClick={() => router.back()} size="lg" className='bg-neutral-950 text-white hover:bg-neutral-800'>
+            Go back
+          </Button>
+          <Button
+            onClick={() => router.push('/dashboard')}
+            size="lg"
+            className='bg-white text-neutral-950 hover:bg-neutral-200'
+          >
+            Back to Home
+          </Button>
+        </div>
       </div>
     </div>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Marketing from './(marketing)/page'
+// import Banner from "../components/Banner";
+import CTASection from "./components/CTASection";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Users from "./components/Users";
+import Footer from "./components/Footer";
 
-const Home = () => {
+export default function Marketing() {
   return (
-    <div>
-      <Marketing />
+    <div className="bg-neutral-950 overflow-hidden scrollbar-hide">
+      {/* <Banner /> */}
+      <Navbar />
+      <Hero />
+      <Users />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <CTASection />
+      <Footer />
     </div>
-  )
+  );
 }
-
-export default Home

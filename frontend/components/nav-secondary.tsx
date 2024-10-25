e4215f4ad1d1de32c431e717116960bae21dacbc
Tanish Singhal <tanishsinghal510@gmail.com>
2024-10-26 00:00:00 +0530

    dark mode added

--- frontend/components/nav-secondary.tsx
+++ frontend/components/nav-secondary.tsx
@@ -8,6 +8,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { ModeToggle } from "@/app/components/ModeToggle"
 
 export function NavSecondary({
   items,
@@ -23,6 +24,7 @@ export function NavSecondary({
     <SidebarGroup {...props}>
       <SidebarGroupContent>
         <SidebarMenu>
+          <ModeToggle />
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild size="sm">
